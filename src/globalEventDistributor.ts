2a95964b5bf47572405f6b9e9cbaf1e8729a6905
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2019-03-08 17:02:43 -0500

    feat: Readme doc and code doc addition

--- src/globalEventDistributor.ts
+++ src/globalEventDistributor.ts
@@ -12,7 +12,14 @@ export class GlobalEventDistributor {
     constructor() {
         this.stores = [];
     }
-
+    /**
+     * Returns an object containing an app state.
+     *
+     * If apps' names does not match the appName passed as param it will
+     * return the `undefined` object.
+     *
+     * @return The default return it's the state the one of the registered as parent app.
+     */
     public getState(appTarget: string = '') {
         const searchCondition = appTarget
             ? (store: IDistributorStore) => store.appName === appTarget
@@ -47,12 +54,26 @@ export class GlobalEventDistributor {
         return filteredStore.length > 0;
     }
 
+    /**
+     * Register an Micro UI store into the system.
+     *
+     * @param itIsHostApp should be use just once to register the parent app.
+     * The rest of the time should be omitted.
+     *
+     */
     public registerStore(appName: string, store: Store<any>, itIsHostApp: boolean = false) {
         if (!this.isStoreRegistered(appName)) {
             this.stores.push({ appName, store, itIsHostApp });
         }
     }
 
+    /**
+     * Broadcast event or send event to an specific app.
+     *
+     * @param appTarget is optional. When omitted, the event will be send to the
+     * parent app.
+     * 
+     */
     public dispatch(event: any, appTarget: string = '') {
         if (!event) {
             return false;
